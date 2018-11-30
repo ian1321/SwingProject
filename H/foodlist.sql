77cc524775186c5f554dfb73fe94d52cb2ed6bdb
ian1321 <44991463+ian1321@users.noreply.github.com>
2018-11-30 16:06:07 +0900

    Create foodlist.sql

---
+++ H/foodlist.sql
@@ -0,0 +1,40 @@
+show databases;
+
+select * from foodlist;
+insert into 
+
+create table foodlist(
+	location VARCHAR(20),
+	sort VARCHAR(20),
+	rest VARCHAR(20),
+	menu VARCHAR(20),
+	price INT
+); 
+
+insert into foodlist values("seoul","chicken","bhc","onion",16000);
+insert into foodlist values("seoul","chicken","bbq","onion",16000);
+insert into foodlist values("seoul","chicken","kfc","fried",16000);
+insert into foodlist values("seoul","chicken","bbq","fried",15900);
+insert into foodlist values("seoul","chicken","bhc","fried",15900);
+insert into foodlist values("seoul","chicken","bhc","seasoned",16000);
+insert into foodlist values("seoul","chicken","kfc","onion",16000);
+insert into foodlist values("seoul","chicken","kfc","seasoned",16000);
+insert into foodlist values()"seoul","chicken","bbq","seasoned",16000);
+insert into foodlist values("seoul","pizza","domino","cheese",20000);
+insert into foodlist values("seoul","pizza","domino","steak",21000);
+insert into foodlist values("seoul","pizza","domino","bulkano",22000);
+insert into foodlist values("seoul","pizza","pizzahut","sweetpotato",22000);
+insert into foodlist values("seoul","pizza","pizzahut","potato",23000);
+insert into foodlist values("seoul","pizza","pizzahut","albolo",25000);
+insert into foodlist values("seoul","pizza","ogurice","shrimp",23500);
+insert into foodlist values("seoul","pizza","ogurice","crab",23500);
+insert into foodlist values("seoul","pizza","ogurice","corn",24500);
+insert into foodlist values("seoul","chinese","kyodong","jajang",3000);
+insert into foodlist values("seoul","chinese","kyodong","jjambbong",4000);
+insert into foodlist values("seoul","chinese","kyodong","tangsuyuk",10000);
+insert into foodlist values("seoul","chinese","backs","jjajang",4000);
+insert into foodlist values("seoul","chinese","backs","jjambbong",5000);
+insert into foodlist values("seoul","chinese","backs","friedrice",5500);
+insert into foodlist values("seoul","chinese","hongkong","friedrice",6000);
+insert into foodlist values("seoul","chinese","hongkong","palbochae",20000);
+insert into foodlist values("seoul","chinese","hongkong","jabchaerice",20000);
