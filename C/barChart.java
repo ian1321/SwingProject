543fa0ce1bc49d9e1dabc8ac42971d30f6b86430
ian1321 <44991463+ian1321@users.noreply.github.com>
2018-12-18 14:09:05 +0900

    Add files via upload

--- C/barChart.java
+++ C/barChart.java
@@ -1,41 +1,35 @@
 package swing;
 
+import java.util.ArrayList;
+
 public class barChart {
-	public String getPieChart() {
-		String htmlString = "<script type=\"text/javascript\" src=\"https://www.gstatic.com/charts/loader.js\"></script>\r\n" + 
-				"  <script type=\"text/javascript\">\r\n" + 
-				"    google.charts.load(\"current\", {packages:['corechart']});\r\n" + 
-				"    google.charts.setOnLoadCallback(drawChart);\r\n" + 
-				"    function drawChart() {\r\n" + 
-				"      var data = google.visualization.arrayToDataTable([\r\n" + 
-				"        [\"Element\", \"Density\", { role: \"style\" } ],\r\n" + 
-				"        [\"9\", 9, \"#b87333\"],\r\n" + 
-				"        [\"10\", 8, \"silver\"],\r\n" + 
-				"        [\"11\", 20, \"gold\"],\r\n" + 
-				"        [\"12\", 3, \"color: #e5e4e2\"]\r\n" + 
-				"      ]);\r\n" + 
+	public String getBarChart(ArrayList<PieElement> list) {
+		String htmlString = "<html>\r\n" + 
+				"  <head>\r\n" + 
+				"    <script type=\"text/javascript\" src=\"https://www.gstatic.com/charts/loader.js\"></script>\r\n" + 
+				"    <script type=\"text/javascript\">\r\n" + 
+				"      google.charts.load(\"current\", {packages:[\"imagebarchart\"]});\r\n" + 
+				"      google.charts.setOnLoadCallback(drawChart);\r\n" + 
 				"\r\n" + 
-				"      var view = new google.visualization.DataView(data);\r\n" + 
-				"      view.setColumns([0, 1,\r\n" + 
-				"                       { calc: \"stringify\",\r\n" + 
-				"                         sourceColumn: 1,\r\n" + 
-				"                         type: \"string\",\r\n" + 
-				"                         role: \"annotation\" },\r\n" + 
-				"                       2]);\r\n" + 
+				"      function drawChart() {\r\n" + 
 				"\r\n" + 
-				"      var options = {\r\n" + 
-				"        title: \"개월당 주문량\",\r\n" + 
-				"        width: 127,\r\n" + 
-				"        height: 129,\r\n" + 
-				"        bar: {groupWidth: \"95%\"},\r\n" + 
-				"        legend: { position: \"none\" },\r\n" + 
-				"		 chartArea:{left:0,top:10,width:\"100%\",height:\"100%\"}" +
-				"      };\r\n" + 
-				"      var chart = new google.visualization.ColumnChart(document.getElementById(\"columnchart_values\"));\r\n" + 
-				"      chart.draw(view, options);\r\n" + 
-				"  }\r\n" + 
-				"  </script>\r\n" + 
-				"<div id=\"columnchart_values\" style=\"width: 132px; height: 134px;\"></div>";
+				"        var data = google.visualization.arrayToDataTable([\r\n" + 
+				"          ['', ''],\r\n";
+				for (int i = 0; i < list.size(); i++) {
+					htmlString += "['" + list.get(i).getName() + "', " + list.get(i).getValue() + "],\r\n";
+				}
+				htmlString +="        ]);\r\n" + 
+				"\r\n" + 
+				"        var chart = new google.visualization.ImageBarChart(document.getElementById('chart_div'));\r\n" + 
+				"\r\n" + 
+				"        chart.draw(data, {width: 132, height: 134, min: 0});\r\n" + 
+				"      }\r\n" + 
+				"    </script>\r\n" + 
+				"  </head>\r\n" + 
+				"  <body>\r\n" + 
+				"    <div id=\"chart_div\" style=\"width: 132px; height: 134px;\"></div>\r\n" + 
+				"  </body>\r\n" + 
+				"</html>";
 		return htmlString;
 	}
 }
